e010f3ba5b699b178bb9963bc5d050aae07bb16b
Kenneth <kennethchiayj@gmail.com>
2026-08-21 15:47:42 +0800

    Added echo functionality for Level-1

--- src/main/java/Sumo.java
+++ src/main/java/Sumo.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 /**
- * Starts Sumo and displays its initial greeting.
+ * Starts Sumo and handles commands entered by the user.
  */
 public class Sumo {
     public static void main(String[] args) {
@@ -15,7 +17,20 @@ public class Sumo {
         System.out.println("Hello! I'm Sumo.");
         System.out.println("What can I do for you?");
         System.out.println(separator);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(separator);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            System.out.println(separator);
+
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(separator);
+                break;
+            }
+
+            System.out.println(" " + command);
+            System.out.println(separator);
+        }
     }
 }
